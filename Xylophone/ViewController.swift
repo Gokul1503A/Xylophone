c7d274a422fa01adea612179ba81a7dab5e7d975
KOPPOLA GOKUL SAI <koppolagokulsai@KOPPOLAs-MacBook-Air.local>
2024-01-02 16:40:44 +0530

    Final Xylophone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func cButton(_ sender: UIButton) {
+        
+        //print(sender.titleLabel? .text as Any)
+        playSound(whatToplay: sender.currentTitle!)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.3){
+            sender.alpha = 1
+        }
+    }
     
-
+    func playSound(whatToplay: String) {
+        
+        print("start")
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.2){
+            print("end")
+        }
+        
+        let url = Bundle.main.url(forResource: whatToplay, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
